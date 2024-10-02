4eefa3d660e6d6985b5d9d2be9df19c1a64fff84
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-10-02 15:11:55 +0700

    Initial Commit

---
+++ SwiftSpeak/SwiftSpeakApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftSpeakApp.swift
+//  SwiftSpeak
+//
+//  Created by Nguyen Phuc Loi on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpeakApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
